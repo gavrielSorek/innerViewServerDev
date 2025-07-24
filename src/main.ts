90b4f586c04f453edd870e97681085350b26b7ab
gavrielSorek <72670796+gavrielSorek@users.noreply.github.com>
2025-07-24 03:07:10 +0300

    server works locally

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable Cross-Origin Resource Sharing (CORS)
+  app.enableCors();
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
